92d0816fe28e0dba1edd4bd11a55544e905fd542
crvicek <crvicek@yahoo.com>
2021-05-08 14:27:30 +0200

    feat: add socket and some debugs

--- src/app.js
+++ src/app.js
@@ -1,13 +1,26 @@
 const config  = require('config')
+const debug   = require('debug')('app:')
 const http    = require('http')
 const express = require('express')
+const WS      = require('ws')
 
 module.exports = function () {
   const app    = express()
   const server = http.createServer(app)
+  const wss    = new WS.Server({ server, path: '/socket' })
 
   app.get('/health', (req, res) => {
     res.send('OK')
+  })
+
+  wss.on('connection', socket => {
+    debug('New client connected')
+    socket.on('message', msg => {
+      debug('Message received', msg)
+    })
+    socket.on('close', () => {
+      debug('Client disconnected')
+    })
   })
 
   const start = () => {
